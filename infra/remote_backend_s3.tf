40f1828fb5650ebd948f381ca20edf94678f2b0f
nalinichandra03 <102302732+nalinichandra03@users.noreply.github.com>
2025-08-10 19:23:34 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "	dev-proj-1-jenkins-remote-state-bucket-29072026"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-29072026"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
